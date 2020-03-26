5fd1bf2da74b476678475ee23afc16d6ef6a8fec
Adam <akb561@york.ac.uk>
2020-03-26 23:45:49 +0100

    stopped using deprecated constructor

--- src/main/java/infobot/App.java
+++ src/main/java/infobot/App.java
@@ -1,5 +1,7 @@
 package infobot;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +14,9 @@ import infobot.mc.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 
 
@@ -34,7 +39,20 @@ public class App extends JavaPlugin
         
         try {
             
-            jda = new JDABuilder("TOKEN").addEventListeners(new DiscordBot())
+            Collection<GatewayIntent> intents = new ArrayList<GatewayIntent>();
+            intents.add(GatewayIntent.GUILD_MESSAGES);
+            Collection<CacheFlag> disabled_caches = new ArrayList<CacheFlag>();
+            disabled_caches.add(CacheFlag.ACTIVITY);
+            disabled_caches.add(CacheFlag.CLIENT_STATUS);
+            disabled_caches.add(CacheFlag.MEMBER_OVERRIDES);
+            disabled_caches.add(CacheFlag.VOICE_STATE);
+            disabled_caches.add(CacheFlag.EMOTE);
+
+            jda =  JDABuilder
+            .create("TOKEN", intents)
+            .setMemberCachePolicy(MemberCachePolicy.NONE)
+            .disableCache(disabled_caches)
+            .addEventListeners(new DiscordBot())
             .setActivity(Activity.playing("Type !ping"))
             .build();
 
