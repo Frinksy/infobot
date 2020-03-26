04a8441acbc063ab916db7ff59b954b5011a7e0f
Adam <akb561@york.ac.uk>
2020-03-27 00:57:14 +0100

    removed genmap command

--- src/main/java/infobot/App.java
+++ src/main/java/infobot/App.java
@@ -31,7 +31,6 @@ public class App extends JavaPlugin
     public void onEnable () {
         getLogger().info("Hello SpigotMC!!");
         this.getCommand("list-players").setExecutor(new CommandListPlayers());
-        this.getCommand("genmap").setExecutor(new MapMaker());
         getServer().getPluginManager().registerEvents(new WeatherListener(), this);
         getServer().getPluginManager().registerEvents(new DiamondFinderListener(), this);
         
