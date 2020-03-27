f7f7729504dfbb6389c0c60f5c74f95f09b8e8d1
Adam <akb561@york.ac.uk>
2020-03-27 13:38:02 +0100

    added space

--- src/main/java/infobot/mc/DiamondFinderListener.java
+++ src/main/java/infobot/mc/DiamondFinderListener.java
@@ -14,7 +14,7 @@ public class DiamondFinderListener implements Listener {
     public void onBlockBreakEvent (BlockBreakEvent event) {
         Block broken_block = event.getBlock();
         if (broken_block.getType() == Material.DIAMOND_ORE) {
-            Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + "has mined a diamond ore!");
+            Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " has mined a diamond ore!");
         }
     }
 
